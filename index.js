0dd08e1424e004208d62c60419e673ebc011eea5
juanda64 <juandavpenilla@gmail.com>
2024-10-25 18:34:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -16,3 +16,7 @@ const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
+
+app.get('/', (req, res) => {
+    res.send('Bienvenido a la API de Doritos');
+});
